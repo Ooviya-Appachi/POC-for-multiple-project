c33e635c46124a0a0b74d7ab647d51110f57c9a3
Ooviya <Ooviya@sharechat.co>
2019-12-09 13:35:51 +0530

    Sample

---
+++ POC/src/main/java/co/poc/Main.java
@@ -0,0 +1,13 @@
+package co.poc;
+
+import poc1.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		demo.printHello();
+	}
+
+}
---
+++ poc1/src/poc1/demo.java
@@ -0,0 +1,7 @@
+package poc1;
+
+public class demo {
+public static void printHello() {
+	System.out.println("Hello");
+}
+}
