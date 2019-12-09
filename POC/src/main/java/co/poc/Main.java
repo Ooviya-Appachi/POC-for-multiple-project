1af21184cde1c7b6e2760ea12bf4dbc986aebbe2
Ooviya <Ooviya@sharechat.co>
2019-12-09 14:37:17 +0530

    New src

--- POC/src/main/java/co/poc/Main.java
+++ POC/src/main/java/co/poc/Main.java
@@ -1,14 +1,12 @@
 package co.poc;
 
 import co.newsrc.Demo;
-import poc1.demo;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
-		demo.printHello();
 		Demo.printHello();
 	}
 
